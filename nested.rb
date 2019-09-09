adb03d72e4281862cc50ae140a0bccd037696494
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-09-09 02:22:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,7 +110,7 @@ def changing_alan
      alans_new_info = "GUI"
      programmer_hash [:known_for] ="alans_new_info"
      programmer_hash[:alans_new_info] ="GUI"
-     
+      programmer_hash
 end
 
 def adding_to_dennis
